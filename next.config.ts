728b2de310c4e057838abba49124727459a23ed6
priyansh-priyansh <red.priyansh@gmail.com>
2025-02-14 00:36:48 +0530

    Online Users

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  typescript: {
+    ignoreBuildErrors: true, 
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    config.infrastructureLogging = { level: "error" }; // Suppresses webpack warnings in dev
+    return config;
+  },
 };
 
 export default nextConfig;
