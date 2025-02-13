fd1cad153c8b33ade56a8f082c064c13790b7799
priyansh-priyansh <red.priyansh@gmail.com>
2025-02-13 20:14:52 +0530

    Initial commit

---
+++ data/index.ts
@@ -0,0 +1,214 @@
+
+
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently leveling up in structured web development.",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Zentry",
+      des: "Explore the wonders of Zentry with a captivating GSAP-powered design.",
+      img: "/Zentry.png",
+      iconLists: ["/re.svg", "/tail.svg", "/js.svg", "/gsap.svg", "/css.svg"],
+      link: "https://lemonchiffon-cheetah-361541.hostingersite.com",
+    },
+    {
+      id: 2,
+      title: "Airbnb",
+      des: "Seamlessly book unique stays with Airbnb's intuitive, modern UI.",
+      img: "/Airbnb.avif",
+      iconLists: ["/css.svg", "/js.svg", "/nodejs.svg", "/mdb.svg", "/bootstrap.svg"],
+      link: "https://airbnb-chi-seven.vercel.app/listings",
+    },
+    {
+      id: 3,
+      title: "Modern Portfolio",
+      des: "Showcase your work with a sleek, interactive portfolio powered by Next.js & Three.js.",
+      img: "/Untitled.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/next.svg"],
+      link: "https://priyanshweb.com/",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Working with Priyansh was a game-changer for our business. His ability to translate complex ideas into seamless web experiences is unmatched. The website he built for us exceeded expectations in both design and functionality.",
+      name: "Sarah Thompson",
+      title: "CEO of BrightVision Media",
+      image: "/testimonial1.png"
+    },
+    {
+      quote:
+        "Priyansh's expertise in web development and UI/UX design brought our platform to life. His attention to detail and commitment to perfection ensured that our site is not only visually stunning but also highly performant.",
+      name: "James Carter",
+      title: "Founder of NextGen Solutions",
+      image: "/profile.svg"
+    },
+    {
+      quote:
+        "From concept to execution, Priyansh demonstrated an incredible level of skill and professionalism. His ability to optimize performance while maintaining a sleek aesthetic made a huge impact on our online presence.",
+      name: "Emily Rodriguez",
+      title: "Marketing Director at PixelWave Studios",
+      image: "/testimonial4.jpg"
+    },
+    {
+      quote:
+        "Priyansh's technical proficiency and problem-solving skills are top-notch. He transformed our outdated website into a modern, responsive, and user-friendly platform, boosting engagement and conversions.",
+      name: "Daniel Foster",
+      title: "CTO of ElevateTech",
+      image: "/testimonial3.webp"
+    },
+    {
+      quote:
+        "Priyansh doesn’t just build websites—he crafts experiences. His ability to blend cutting-edge technology with creative design made our platform stand out in a crowded market. Highly recommend working with him!",
+      name: "Olivia Martinez",
+      title: "Creative Director at Visionary Hub",
+      image: "/testimonial5.jpeg"
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Alpha (DSA with Java)",
+      desc: "Completed the Alpha (DSA with Java) course by Apna College.",
+      className: "md:col-span-2",
+      thumbnail: "/Alpha.png",
+    },
+    {
+      id: 2,
+      title: "Mobile App Dev - JSM Tech",
+      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      className: "md:col-span-2", 
+      thumbnail: "/Delta.png",
+    },
+    {
+      id: 3,
+      title: "Freelance App Dev Project",
+      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      className: "md:col-span-2", 
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Lead Frontend Developer",
+      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link: "https://github.com/priyansh-priyansh"
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      link: "https://www.linkedin.com/in/priyansh-bansal/",
+    },
+  ];
